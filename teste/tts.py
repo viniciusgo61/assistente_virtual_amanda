9f95e7163a0bf52a837408544ddb62a9e283763a
Vinicius Gomes <viniciusgomes05@hotmail.com>
2021-05-17 19:30:31 -0300

    finalizando commit 0.4

--- teste/tts.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-
-
-voices = engine.getProperty('voices')
-engine.setProperty('voice',voices[-2].id)
-
-engine.say("acabou a água, toma banho de leite")
-engine.runAndWait()
